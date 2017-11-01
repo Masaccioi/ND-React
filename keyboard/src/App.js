cd075e0c1494e7dc5da1e3ee4916f4957de4a03c
213520 <213520@nd.com>
2017-11-01 15:47:44 +0800

    mask淡入

--- keyboard/src/App.js
+++ keyboard/src/App.js
@@ -1,93 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import closeIcon from './img/redenvelopel_close_icon_normal.png'
-import backspaceIcon from './img/redenvelopel_keyboard_Backspace_normal.png'
-
-class Keyboard extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      result:[]
-    }
-    // this.props.onClick = this.props.onClick.bind(this)
-  }
-  render() {
-    return (
-      <div>
-        <div className='numberKeyboardHeader'>
-          <img src={ closeIcon } className= { 'closeIcon' } onClick={ this.props.onCancel }/>
-          <p className='numberKeyboardTitle'>{ this.props.title }</p>
-        </div>
-        <div className='numberList'>
-          <div className={ 'numberListWrapper' }>
-            {
-              [0,1,2,3,4,5].map(i => {
-                return (
-                  <div className={ 'numberItem' + ' ' + (this.state.result.length > 0 ? 'active' : '') }><span>{this.state.result.length > i ? '*' : '' }</span></div>
-                )
-              })
-            }
-          </div>
-        </div>
-        <table className='numberKeyboardBody'>
-          <tbody>
-            <tr>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>1</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>2</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>3</td>
-            </tr>
-            <tr>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>1</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>2</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>3</td>
-            </tr>
-            <tr>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>1</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>2</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('1') }>3</td>
-            </tr>
-            <tr>
-              <td className={ 'numberKeyboardItem' }></td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.props.onClick('0') }>0</td>
-              <td className={ 'numberKeyboardItem' } onClick={ this.onDelete }>
-                  <img className={ 'backspaceIcon' } src={ backspaceIcon }/>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-}
-
+import Password from './password/password.js'
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      groupVisibled: false
+      visible: false,
+      value:'',
+      title: '请输入支付密码'
     }
-    this.onClick = () => {}
+    this.onClick = this.onClick.bind(this)
+    this.onCancel = this.onCancel.bind(this)
+    this.onChange = this.onChange.bind(this)
   }
+
+  onCancel () {
+    this.setState({
+      visible: false
+    })
+  }
+
+  onClick () {
+    this.setState({
+      visible: true
+    })
+  }
+
+  onChange (value) {
+    if (value.length === 6) {
+      if (value !== '123456') {
+        this.setState({
+          value: '',
+          title: '请再次输入支付密码'
+        })
+      } else {
+        this.setState({
+          value: '',
+          visible: false
+        })
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={() => { this.setState({ groupVisibled: !this.state.groupVisibled })}}>click</button>
-        {
-          this.state.groupVisibled ? 
-            <ReactCSSTransitionGroup
-            transitionName="example"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <h1>Fading at Initial Mount</h1>
-          </ReactCSSTransitionGroup> : ''
-        } 
-        <Keyboard onClick={() => this.onClick()}/>
+      <div>
+        <button onClick={ () => this.onClick() }>click</button>
+        <Password
+          value={ this.state.value }
+          visible={ this.state.visible }
+          onChange={ this.onChange }
+          onCancel={ this.onCancel }
+          title={ this.state.title }/> 
       </div>
     );
   }
--- keyboard/src/password/password.js
+++ keyboard/src/password/password.js
@@ -1,156 +1,136 @@
-import React, { PropTypes } from 'react'
-import { intlShape, defineMessages, FormattedMessage } from 'react-intl'
-import IntlUtil from 'i18n/intlUtil'
-import styles from './password.css'
-import Popup from 'antd-mobile/lib/popup'
+import React, { Component } from 'react';
+import './password.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import closeIcon from './img/redenvelopel_close_icon_normal.png'
 import backspaceIcon from './img/redenvelopel_keyboard_Backspace_normal.png'
-const isIPhone = new RegExp('\\biPhone\\b|\\biPod\\b', 'i').test(window.navigator.userAgent)
-let maskProps
-if (isIPhone) {
-  // Note: the popup content will not scroll.
-  maskProps = {
-    onTouchStart: e => e.preventDefault()
+
+class Keyboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      result: props.value
+    }
+    this.onClick = this.onClick.bind(this)
+    this.onDelete = this.onDelete.bind(this)
+  }
+
+  onDelete () {
+    let str = this.state.result
+    if (str.length === 0) {
+      return
+    }
+    str = str.substr(0, str.length - 1)
+    this.setState({ result: str })
+    this.props.onChange(str)
+  }
+
+  onClick (value) {
+    let str = this.state.result
+    if (str.length < 6) {
+        str = str + value
+        this.setState({ result: str })
+        this.props.onChange( str )
+    } else {
+        return
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ result: nextProps.value })
+  }
+
+  renderNumber(i) {
+    if (i === 10) {
+      return (
+        <td className={ 'numberKeyboardItem' }></td>
+      )
+    } else if (i === 11){
+      return (
+        <td className={ 'numberKeyboardItem' } onClick={ () => this.onDelete() }>
+          <img className={ 'backspaceIcon' } src={ backspaceIcon }/>
+        </td>
+      )
+    } else {
+      return (
+        <td className={ 'numberKeyboardItem' } onClick={ () => this.onClick(i) }>{i}</td>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='numberKeyboardHeader'>
+          <img src={ closeIcon } className= { 'closeIcon' } onClick={ this.props.onCancel }/>
+          <p className='numberKeyboardTitle'>{ this.props.title }</p>
+        </div>
+        <div className='numberList'>
+          <div className={ 'numberListWrapper' }>
+            {
+              [0,1,2,3,4,5].map(i => {
+                return (
+                  <div key={i} className={ 'numberItem' + ' ' + (this.state.result.length > 0 ? 'active' : '') }><span>{this.state.result.length > i ? '*' : '' }</span></div>
+                )
+              })
+            }
+          </div>
+        </div>
+        <table className='numberKeyboardBody'>
+          <tbody>
+            <tr>
+              {this.renderNumber(1)}
+              {this.renderNumber(2)}
+              {this.renderNumber(3)}
+            </tr>
+            <tr>
+              {this.renderNumber(4)}
+              {this.renderNumber(5)}
+              {this.renderNumber(6)}
+            </tr>
+            <tr>
+              {this.renderNumber(7)}
+              {this.renderNumber(8)}
+              {this.renderNumber(9)}
+            </tr>
+            <tr>
+              {this.renderNumber(10)}
+              {this.renderNumber(0)}
+              {this.renderNumber(11)}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
-class Keyboard extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            result: props.value
-        }
-        this.onClick = this.onClick.bind(this)
-        this.onDelete = this.onDelete.bind(this)
+class Password extends Component {
+  constructor(props){
+    super(props)
+    this.maskClose = this.maskClose.bind(this)
+  }
+  maskClose () {
+    if (this.props.maskClose || !this.props.maskClose) {
+      this.props.onCancel()
     }
-    
-    onDelete () {
-        let str = this.state.result
-        str = str.substr(0, str.length - 1)
-        this.setState({ result: str })
-        this.props.onChange(str)
-    }
+  }
 
-    onClick (value) {
-        let str = this.state.result
-        if (str.length < 6) {
-            str = str + value
-            this.setState({ result: str })
-            this.props.onChange( str )
-        } else {
-            return
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({ result: nextProps.value })
-    }
-
-    render() {
-        return (<div>
-            <div className={ styles.numberKeyboardHeader }>
-                <img src={ closeIcon } className= { styles.closeIcon } onClick={ this.props.onCancel }/>
-                <p className={ styles.numberKeyboardTitle }>{ this.props.title }</p>
-            </div>
-            <div className={ styles.numberList }>
-                <div className={ styles.numberListWrapper }>
-                    {/* todo 用循环渲染 */}
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 0 ? '*' : '' }</span></div>
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 1 ? '*' : '' }</span></div>
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 2 ? '*' : '' }</span></div>
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 3 ? '*' : '' }</span></div>
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 4 ? '*' : '' }</span></div>
-                    <div className={ styles.numberItem + ' ' + (this.state.result.length > 0 ? styles.active : '') }><span>{this.state.result.length > 5 ? '*' : '' }</span></div>
-                </div>
-            </div>
-            <table className={ styles.numberKeyboardBody } >
-                <tbody>
-                    <tr>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '1') }>1</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '2') }>2</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '3') }>3</td>
-                    </tr>
-                    <tr>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '4') }>4</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '5') }>5</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '6') }>6</td>
-                    </tr>
-                    <tr>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '7') }>7</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '8') }>8</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '9') }>9</td>
-                    </tr>
-                    <tr>
-                        <td className={ styles.numberKeyboardItem }></td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onClick.bind(this, '0') }>0</td>
-                        <td className={ styles.numberKeyboardItem } onClick={ this.onDelete }>
-                            <img className={ styles.backspaceIcon } src={ backspaceIcon }/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>)
-    }
-}
-Keyboard.propTypes = {
-    onChange: PropTypes.func,
-    onCancel: PropTypes.func,
-    title: PropTypes.string,
-    value: PropTypes.string
-}
-export default class Password extends React.Component {
-
-    constructor(props) {
-        super(props)
-        const self = this
-        this.onClear = (value, title) => {
-            Popup.show(<Keyboard
-                onChange={ this.props.onChange } 
-                onCancel={ this.props.onCancel }
-                value={ value }
-                title={ title || this.props.title }/>, { animationType: 'slide-up', maskProps, onMaskClose: this.props.onCancel, maskClosable:true })
-        }
-    }
-
-    componentDidMount() {
-        if(this.props.visible) {
-            Popup.show(<Keyboard 
-                onChange={ this.props.onChange } 
-                onCancel={ this.props.onCancel }
-                value={ this.props.value }
-                title={ this.props.title }/>, { animationType: 'slide-up', maskProps, onMaskClose: this.props.onCancel, maskClosable:true })
-        } else {
-            Popup.hide()
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if(!this.props.visible && nextProps.visible) {
-            Popup.show(<Keyboard 
-                onChange={ nextProps.onChange } 
-                onCancel={ nextProps.onCancel }
-                value={ nextProps.value }
-                title={ nextProps.title }/>, { animationType: 'slide-up', maskProps, onMaskClose: nextProps.onCancel, maskClosable:true })
-        } 
-        if (this.props.visible && !nextProps.visible) {
-            Popup.hide()
-        }
-        
-    }
-    
-    componentWillUnmount() {
-        Popup.hide()
-    }
-
-    render() {
-        return <div />
-    }
+  render() {
+    return (
+      <div>
+        <div className={'password' + ' ' + (this.props.visible ? 'show' : '')}>
+          <div className='mask' onClick={() => this.maskClose() }></div>
+          <div className={'keyboard' + ' ' + (this.props.visible ? 'in' : 'out')}>
+            <Keyboard onChange={this.props.onChange}
+              onCancel={this.props.onCancel}
+              title={this.props.title}
+              value={this.props.value}
+            />
+          </div>
+        </div> 
+      </div>
+    );
+  }
 }
 
-Password.propTypes = {
-    visible: PropTypes.bool,
-    title: PropTypes.string,
-    onChange: PropTypes.func,
-    onCancel: PropTypes.func,
-    value: PropTypes.string
-}
+export default Password;
